1867de8ece2b7f46fad54da73b48b53c74027892
yuvalt30 <72381398+yuvalt30@users.noreply.github.com>
2021-04-01 22:13:04 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuvalt30/advanced-programming
-# Build directory: /home/yuvalt30/advanced-programming/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
